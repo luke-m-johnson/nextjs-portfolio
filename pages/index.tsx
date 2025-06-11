525ec375e3a630fdab9cd1f0b005265974f011e4
luke-m-johnson <mail@lukejohnson.info>
2025-06-12 01:17:00 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,21 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { motion } from 'framer-motion';
+import { MotionH1 } from '../components/Motion';
 
 export default function Home() {
   return (
     <>
-      <header>{/* Your Header here */}</header>
       <main className="min-h-screen p-8">
-        <motion.h1
+        <MotionH1
           className="text-4xl font-bold"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
         >
           Welcome to My Portfolio
-        </motion.h1>
+        </MotionH1>
       </main>
     </>
   );
 }
+
